78900ce34c8998ce4c33929f82b486c6483fa2b6
alslemos <alexandre-ls@hotmail.com>
2024-03-19 13:15:23 -0300

    homeRouter ok

---
+++ StartingWithViper/HomeRouter.swift
@@ -0,0 +1,26 @@
+//
+//  HomeRouter.swift
+//  StartingWithViper
+//
+//  Created by Alexandre Lemos da Silva on 19/03/24.
+//
+
+import Foundation
+import UIKit
+
+protocol HomeRouterInterface: AnyObject { }
+
+final class HomeRouter: HomeRouterInterface {
+    
+    static func createModule(using navigationController: UINavigationController) -> HomeViewController {
+            let router = HomeRouter()
+            guard let view = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "homeVC") as? HomeViewController else { return HomeViewController() }
+            let interactor = HomeInteractor()
+            let presenter = HomePresenter(view: view, router: router, interactor: interactor)
+            
+            view.presenter = presenter
+            return view
+        }
+}
+
+
