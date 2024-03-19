eca602b4755978a901d9a64194dc1edea72593ca
alslemos <alexandre-ls@hotmail.com>
2024-03-18 22:51:51 -0300

    LocalService base

---
+++ StartingWithViper/LocalService.swift
@@ -0,0 +1,53 @@
+//
+//  LocalService.swift
+//  StartingWithViper
+//
+//  Created by Alexandre Lemos da Silva on 18/03/24.
+//
+
+import Foundation
+import UIKit
+
+final class LocalService {
+    static let shared: LocalService = LocalService()
+    
+    func saveDummyData(){
+        
+        let dataKey = "dummyDatas"
+        var dummyDatas: [BaseModel] = []
+        
+        for number in 0...10{
+            dummyDatas.append(BaseModel(title: "DummyTittle\(number)", description: "DummyDescription\(number)"))
+        }
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(dataKey)
+            
+            do {
+                if let encoded = try? JSONEncoder().encode(dummyDatas) {try encoded.write(to: fileURL)}
+            } catch  {
+                fatalError("an error was taken on save progress")
+            }
+            
+        }
+    }
+    
+    func realData() -> [BaseModel]? {
+        let dataKey = "dummyDatas"
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(dataKey)
+            do {
+                let stringContent = try String(contentsOf: fileURL, encoding: .utf8)
+                if  let decoded = try? JSONDecoder().decode([BaseModel].self, from: stringContent.data(using: .utf8) ?? Data()){
+                    return decoded
+                }
+            } catch  {
+                fatalError("An error was taken on read progress")
+            }
+        }
+        return []
+    }
+    
+}
+
+
